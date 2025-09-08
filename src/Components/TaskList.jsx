03a8209ea32dd94e3921662776298aefe2edda88
Emmanuelfwd <eruizhurtado25fwd@gmail.com>
2025-09-08 12:48:38 -0600

    correccion de codigo

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,47 +1,26 @@
-import React from 'react'
-import { getTareas, agregarTarea, eliminarTarea, actualizarTarea } from '../Services/Services';
+import React, { useState, useEffect } from 'react'
+import { getTareas, agregarTarea, eliminarTarea, actualizarTarea } from '../Services/Services'
 
-useEffect(() => {
-  const cargarTareas = async () => {
-    try {
-      const lista = await getTareas()
-      setTareas(lista)
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
-  cargarTareas()
-}, [])
+export default function TaskList() {
+  const [tareas, setTareas] = useState([]);
 
-function TaskList() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const lista = await getTareas();
+        setTareas(lista);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
-     <div>
-      <div>
-        <input type="text" placeholder="Ingresar tarea" />
-        <button>Agregar</button>
-      </div>
-
-      <div>
-        <p>Tareas Completadas</p>
-        <div>0</div>
-      </div>
-
-      <div>
-        <div>
-          <input type="checkbox" />
-          <span>Ejemplo de tarea 1</span>
-          <button>Eliminar</button>
-        </div>
-
-        <div>
-          <input type="checkbox" />
-          <span>Ejemplo de tarea 2</span>
-          <button>Eliminar</button>
-        </div>
-      </div>
+    <div>
+      {tareas.map((tarea) => (
+        <div key={tarea.id}>{tarea.nombre}</div>
+      ))}
     </div>
-  )
+  );
 }
-
-export default TaskList
