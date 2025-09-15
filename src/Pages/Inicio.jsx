c6f2d671478884f8d5edcb3c3124d1a8b635d3b1
Emmanuelfwd <eruizhurtado25fwd@gmail.com>
2025-09-15 13:49:17 -0600

    cambios cosmeticos

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Login from '../Components/Login'
+import {motion}  from "motion/react";
 
 function Inicio() {
   return (
     <div>
-    <h1>Bienvenido</h1>
+    <motion.h1
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ type: 'spring', stiffness: 120, damping: 8 }}
+            
+            >Bienvenido</motion.h1>
     <Login />
     </div>
   )
--- src/Pages/Todo.jsx
+++ src/Pages/Todo.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import TaskList from '../Components/TaskList'
+import {motion}  from "motion/react";
+
 
 function Todo() {
   return (
     <div>
       <Navbar />
-      <h1>Lista de Tareas</h1>
+      <motion.h1
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ type: 'spring', stiffness: 120, damping: 8 }}
+        
+        >Lista de Tareas</motion.h1>
       <TaskList />
       
     </div>
